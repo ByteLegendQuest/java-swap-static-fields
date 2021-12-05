6946216b53d46711c7083431ef4eb0cb840f4198
reallEz <jingqing.xj@outlook.com>
2021-12-05 09:00:32 +0000

    Update at 2021-12-05T09:00:32.563828Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,8 +12,9 @@ public class Challenge {
 
     // Complete this method
     public static void swap() {
-        // Test driven develop. >.<
-        a = 2;
-        b = 1;
+        // Well done random number.
+        int c = a;
+        a = b;
+        b = c;
     }
 }

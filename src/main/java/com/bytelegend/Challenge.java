b1280547fc84984c87e88e377847be2bf32001e5
MeikleH <hxhmic@gmail.com>
2021-12-25 14:14:02 +0000

    Update at 2021-12-25T14:14:02.054998Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,5 +11,8 @@ public class Challenge {
     }
 
     // Complete this method
-    public static void swap() {}
-}
+    public static void swap() {
+        int t=a;
+        a=b;
+        b=t;
+    }

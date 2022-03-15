20f3a926027153833372ce6e1d860f1edba4ad09
lemon-altxx <2515782282@qq.com>
2022-03-15 05:01:18 +0000

    Update at 2022-03-15T05:01:18.742245Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,5 +11,9 @@ public class Challenge {
     }
 
     // Complete this method
-    public static void swap() {}
+    public static void swap() {
+        int a = a+b;
+        int b = a-b;
+        int a = a-b;
+    }
 }

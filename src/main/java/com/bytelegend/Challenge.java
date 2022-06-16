77a692b7666e65bef5babd83c9e74ade03cd6088
karolk75 <karolk410410@gmail.com>
2022-06-16 14:58:35 +0000

    Update at 2022-06-16T14:58:35.026807Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,7 +12,8 @@ public class Challenge {
 
     // Complete this method
     public static void swap() {
-        this.a = 2;
-        this.b = 1;
+        int c = this.b;
+        this.a = this.b;
+        this.b = c;
     }
 }

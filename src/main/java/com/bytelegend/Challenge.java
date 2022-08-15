5cbb89361d2a8754c5ea667c03be9c20f8177c6b
NotAWeeaboo <billyyoo052@gmail.com>
2022-08-15 06:39:14 +0000

    Update at 2022-08-15T06:39:14.641630Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,5 +11,9 @@ public class Challenge {
     }
 
     // Complete this method
-    public static void swap() {}
+    public static void swap() {
+        a = 2;
+        b = 1;
+        
+    }
 }

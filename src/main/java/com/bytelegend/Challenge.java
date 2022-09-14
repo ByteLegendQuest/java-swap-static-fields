eb4256f47272045b221cae74445849485aefff94
HeeiYa <m15713694492@163.com>
2022-09-14 00:58:35 +0000

    Update at 2022-09-14T00:58:35.776417Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,18 +1,18 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static int a = 1;
-    public static int b = 2;
+        public static int a = 1;
+        public static int b = 2;
 
-    public static void main(String[] args) {
-        System.out.println("Before swap: a=" + a + ", b=" + b);
-        swap(a,b);
-        System.out.println("After swap: a=" + a + ", b=" + b);
-    }
+        public static void main(String[] args) {
+            System.out.println("Before swap: a=" + a + ", b=" + b);
+            swap(a,b);
+            System.out.println("After swap: a=" + a + ", b=" + b);
+        }
 
-    // Complete this method
-     public static void swap(int num1, int num2) {
+        // Complete this method
+        public static void swap(int num1,int num2) {
             a = num2;
             b = num1;
         }
-}
+    }

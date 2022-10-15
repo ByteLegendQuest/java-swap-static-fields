f26f36fe70d827e25612142cca91fae036b0bc2a
YJiva <2280134514@qq.com>
2022-10-15 12:47:07 +0000

    Update at 2022-10-15T12:47:07.703159Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,5 +11,10 @@ public class Challenge {
     }
 
     // Complete this method
-    public static void swap() {}
+    public static void swap() {
+        int c;
+        c=a;
+        a=b;
+        b=c;
+    }
 }

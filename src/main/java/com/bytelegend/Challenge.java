40ca39aeebbdc4dff93142b0803aa61c273c563f
carlcheung <53021927+carlcheung@users.noreply.github.com>
2022-12-14 12:48:52 +0000

    Update at 2022-12-14T12:48:52.788935Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,7 +12,9 @@ public class Challenge {
 
     // Complete this method
     public static void swap() {
-        a = 2;
-        b = 1;
+        int c;
+        c = a;
+        a = b;
+        b = c;
     }
 }

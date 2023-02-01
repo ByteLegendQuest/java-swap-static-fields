30ce496fed60a0574f12204b3e7873edc065261d
AaronFledermaus <78683536+aaronfledermaus@users.noreply.github.com>
2023-02-01 13:32:06 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,5 +11,8 @@ public class Challenge {
     }
 
     // Complete this method
-    public static void swap() {}
+    public static void swap() {
+        static int c;
+        c=a;a=b;b=c;
+    }
 }

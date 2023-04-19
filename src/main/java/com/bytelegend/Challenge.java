5435867b0cfa859b08944546f7708215eaff2c3e
mijingduI <727539981@qq.com>
2023-04-19 08:52:54 +0000

    Update at 2023-04-19T08:52:54.016085Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -11,5 +11,9 @@ public class Challenge {
     }
 
     // Complete this method
-    public static void swap() {}
+    public static void swap() {
+        int tem = b;
+        b = a;
+        a = tem;
+    }
 }
